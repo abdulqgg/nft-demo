4c5f03a339294f83b3b4f78713f436ab511acdef
abdul gilani <abdul.gilani@gmail.com>
2022-07-20 17:18:00 +0100

    init

---
+++ contracts/SimpleCollectible.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.6.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract SimpleCollectible is ERC721 {
+    constructor() public ERC721 ("Dogie", "DOG"){
+    }
+
+    function createCollectible() public returns (uint256){
+        
+    } 
+    
+}
+
