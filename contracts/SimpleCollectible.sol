1c860b85a3f75a8a89d6fe1402312f0b96e6db9c
abdul gilani <abdul.gilani@gmail.com>
2022-07-20 17:31:38 +0100

    safemint

--- contracts/SimpleCollectible.sol
+++ contracts/SimpleCollectible.sol
@@ -1,15 +1,21 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity 0.6.0;
+pragma solidity 0.6.6;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract SimpleCollectible is ERC721 {
+    uint256 public tokenCounter;
+
     constructor() public ERC721 ("Dogie", "DOG"){
+        tokenCounter = 0;
     }
 
     function createCollectible() public returns (uint256){
-        
+        uint256 tokenId = tokenCounter;
+        _safeMint(msg.sender, tokenId);
+        tokenCounter = tokenCounter + 1;
+        return tokenId;
     } 
     
 }
